8f0dc1663418547560c37e9fa3b6f48754fbbf0c
Joao Xavier <joaobxavier@gmail.com>
2021-04-15 12:24:02 -0400

    Updated the three species model for pertubation

--- ThreeSpeciesLotkaVolterraMatrix_Wendy.m
+++ ThreeSpeciesLotkaVolterraMatrix_Wendy.m
@@ -30,51 +30,70 @@ a = 1;
 c = 0.5;
 
 
-for b = 0.2
+b = 0.2
 %for b = 0:0.2:1
-    % build the vector of growth rates mu 
-    % and the matrix of species-species interaction M
-    mu = [a; b; c];
-    %M = [0 -1 1; 0 0 1; 0 0 0]; %original
-    M = [-0.5 -0.5 1; 0 -0.4 1; 0 0 -0.6]; %add in self inhibition
-    
-    % simulate the ecosystem
-    %[t,y] = ode23(@volterraMatrixForm, [0 100], [0.01 0.01 0.01]);
-    
-    %simulate perturbation that decreases population by 90%
-    [t,y] = ode23(@volterraMatrixForm, [100 200], [0.10827 0.25828 0.08333]);
-    
-    figure(plot_index)
-    
-    plot(t,y, 'LineWidth', 2);
-    title('Microbe Populations Over Time');
-    xlabel('t');
-    ylabel('Population');
-    legend('Species A','Species B', 'Species C', 'Location','North');
-    % Species B is growing exponentially quite fast, because it's helped by
-    % A. C is crashing down because it has a negative growth rate and no
-    % other species to help it. In this case it's better to look at the
-    % population numbers in log scale.
-    
-    % Wendy: Once the self interaction of Species A is fixed, the greater negative
-    % self interaction Species B has, the sooner it reaches equilibrium. Species C
-    % will always be crashing down to extinction with a negative growth rate along 
-    % with self inhibition. The greater the self inhibition, the faster it
-    % crashes down and goes extinct. Although once it when C's self inhibition is
-    % in between -0.5 and -1, it seems like C briefly makes a come back and
-    % then continues to crash down.
-    
-    set(gca, 'YScale', 'log');
-    drawnow; %J added this command to make sure the figure shows
-    
-    % changed the b and d formats from %d to %0.2f
-    % which is a floating-point number with 2 decimal places
-    figure_filename = sprintf('Pred_Prey_matrix_a_%d_b_%0.2f_c_%d.png',a,b,c); %J added semicolumn
-    saveas(figure(plot_index), figure_filename);
-    %J: updating the plot_index needs to come after saving the figure
-    % otherwise the saveas saves and empty figure.
-    plot_index = plot_index + 1; %J: added semicolumn
-end
+% build the vector of growth rates mu
+% and the matrix of species-species interaction M
+mu = [a; b; c];
+%M = [0 -1 1; 0 0 1; 0 0 0]; %original
+M = [-0.5 -0.5 1; 0 -0.4 1; 0 0 -0.6]; %add in self inhibition
+
+% simulate the ecosystem before perturbation
+[t,y] = ode23(@volterraMatrixForm, [0 100], [0.01 0.01 0.01]);
+
+%%
+figure(1)
+subplot(3, 1, 1)
+plot(t, y)
+title('Ecosystem before pertubation')
+
+finalYBeforePertubation =  y(end, :);
+%yAfterPertubation =  finalYBeforePertubation * 0.1;
+yAfterPertubation =  finalYBeforePertubation .* [1 0.1 1];
+
+% simulate perturbation that decreases population by 90%
+[tAfter,yAfter] = ode23(@volterraMatrixForm, [100 200], yAfterPertubation);
+
+figure(1)
+subplot(3, 1, 2)
+plot(tAfter, yAfter)
+title('Ecosystem after pertubation')
+
+%
+tCombined = [t; tAfter];
+yCombined = [y; yAfter];
+
+figure(1)
+subplot(3, 1, 3)
+plot(tCombined, yCombined)
+title('Ecosystem before/after pertubation')
+legend({'Species A', 'Species B', 'Species C'})
+
+
+
+% Species B is growing exponentially quite fast, because it's helped by
+% A. C is crashing down because it has a negative growth rate and no
+% other species to help it. In this case it's better to look at the
+% population numbers in log scale.
+
+% Wendy: Once the self interaction of Species A is fixed, the greater negative
+% self interaction Species B has, the sooner it reaches equilibrium. Species C
+% will always be crashing down to extinction with a negative growth rate along
+% with self inhibition. The greater the self inhibition, the faster it
+% crashes down and goes extinct. Although once it when C's self inhibition is
+% in between -0.5 and -1, it seems like C briefly makes a come back and
+% then continues to crash down.
+
+drawnow; %J added this command to make sure the figure shows
+
+% changed the b and d formats from %d to %0.2f
+% which is a floating-point number with 2 decimal places
+figure_filename = sprintf('Pred_Prey_matrix_a_%d_b_%0.2f_c_%d.png',a,b,c); %J added semicolumn
+saveas(figure(plot_index), figure_filename);
+%J: updating the plot_index needs to come after saving the figure
+% otherwise the saveas saves and empty figure.
+plot_index = plot_index + 1; %J: added semicolumn
+
 
 %% PREY death rate constant, change PREDATOR death rate
 % I deleted this example to keep it simple.
